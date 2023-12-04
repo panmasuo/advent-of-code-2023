138eec2c264433be5ce6d6337c20ed41d83228d0
Paweł Zarembski <pawezarem@gmail.com>
2023-12-04 16:04:59 +0100

    Update and rename day01 to day01.cpp
    add version 2

--- 01/day01.cpp
+++ 01/day01.cpp
@@ -5,9 +5,9 @@
 #include <vector>
 
 // parse string, concatenate first and last digit of input
-std::string concatenate_first_and_last_digit(std::string& input)
+auto concatenate_first_and_last_digit(std::string& input) -> std::string
 {
-    std::regex pattern("[\\d]");
+    std::regex pattern("(?=((one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)|(\\d)))");
 
     auto numbers_begin = std::sregex_iterator(input.begin(), input.end(), pattern);
     auto numbers_end = std::sregex_iterator();
@@ -16,7 +16,7 @@ std::string concatenate_first_and_last_digit(std::string& input)
     std::string second_number{};
 
     std::smatch first_match = *numbers_begin;
-    first_number = first_match.str();
+    first_number = first_match.str(1);
     auto count = std::distance(numbers_begin, numbers_end);
 
     for (int i = 0; i < count - 1; ++i) {
@@ -24,17 +24,25 @@ std::string concatenate_first_and_last_digit(std::string& input)
     }
 
     std::smatch second_match = *numbers_begin;
-    second_number = second_match.str();
+    second_number = second_match.str(1);
 
-    return first_number + second_number;
+    return (first_number.size() > 1 ? mapped_strings.at(first_number) : first_number) +
+           (second_number.size() > 1 ? mapped_strings.at(second_number) : second_number);
 }
 
 // input data
-std::vector<std::string> in{"1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"};
+std::vector<std::string> in{
+    two1nine
+    eightwothree
+    abcone2threexyz
+    xtwone3four
+    4nineeightseven2
+    zoneight234
+    7pqrstsixteen
+};
 
 auto main() -> int
 {
-
     auto sum = std::transform_reduce(in.begin(), in.end(), 0, std::plus{},
         [](std::string& line) { return stoi(concatenate_first_and_last_digit(line)); }
     );
